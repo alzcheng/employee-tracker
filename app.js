ff7e92102ceb03a45f1fdba3a140a3efeb8da1b5
AZC <albertzcheng@gmail.com>
2020-12-18 20:33:20 -0800

    added add Department function to app

--- app.js
+++ app.js
@@ -14,5 +14,80 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log("database connected");
-    connection.end();
+    mainMenu();
 });
+
+const mainMenu = () => {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "Which action would you like to do?",
+        choices: [
+            "Add departments, roles, or employees",
+            "View departments, roles, or employees",
+            "Update employee roles",
+            "Exit program",
+        ],
+    }).then((userResponse) => {
+        console.log(userResponse.action);
+        switch (userResponse.action) {
+            case "Add departments, roles, or employees":
+                addToDB();
+                break;
+            case "View departments, roles, or employees":
+
+                break;
+            case "Update employee roles":
+
+                break;
+
+            default:
+                connection.end();
+                process.exit(0);
+                break;
+        }
+    })
+};
+
+// -------------------- Adding to the Database --------------------------
+const addToDB = () => {
+    inquirer.prompt({
+        name: "added_element",
+        type: "list",
+        message: "Which element do you like to add to the database?",
+        choices: [
+            "Department",
+            "Role",
+            "Employee"
+        ],
+    }).then((userResponse) => {
+        switch (userResponse.added_element) {
+            case "Department":
+                addDepartment();
+                break;
+
+            case "Role":
+                addRole();
+                break;
+
+            default:
+                addEmployee();
+                break;
+        }
+    })
+};
+
+const addDepartment = () => {
+    inquirer.prompt({
+        name: "department_name",
+        type: "input",
+        message: "Please enter the new department name."
+    }).then((userResponse) => {
+        connection.query(
+            "INSERT INTO department (name) VALUE (?)", userResponse.department_name, (err, data) => {
+                if (err) throw err;
+                mainMenu();
+            }
+        )
+    })
+}
