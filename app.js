45215db2250d9507176904bc13a0a4293256dc08
AZC <albertzcheng@gmail.com>
2020-12-18 18:18:28 -0800

    added connections to database

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+//Connect to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employees_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("database connected");
+    connection.end();
+});
---
+++ seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO
+    department (name) VALUE ("Engineering"),
+    ("Finance"),
+    ("Operations");
+
+INSERT INTO
+    role (title, salary, department_id) VALUE ("Engineer", 90000, 1),
+    ("Engineer", 85000, 3),
+    ("Accountant", 120000, 1),
+    ("Accountant", 100000, 2),
+    ("Accountant", 110000, 3);
+
+INSERT INTO
+    employee (first_name, last_name, role_id) VALUE ("Albert", "Cheng", 1);
+
+INSERT INTO
+    employee (first_name, last_name, role_id, manager_id) VALUE ("John", "Smith", 2, 1);
