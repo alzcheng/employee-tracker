c40d5137e6198c70b0e9bc3b218766dc984d2cd1
AZC <albertzcheng@gmail.com>
2020-12-23 22:51:06 -0800

    modified org chart for tests

--- seed.sql
+++ seed.sql
@@ -1,17 +1,30 @@
-INSERT INTO
-    department (name) VALUE ("Engineering"),
-    ("Finance"),
-    ("Operations");
+INSERT INTO department (name) 
+VALUE 
+("Executive")
+("Engineering"),
+("Operations");
 
-INSERT INTO
-    role (title, salary, department_id) VALUE ("Engineer", 90000, 1),
-    ("Engineer", 85000, 3),
-    ("Accountant", 120000, 1),
-    ("Accountant", 100000, 2),
-    ("Accountant", 110000, 3);
+INSERT INTO role (title, salary, department_id) 
+VALUE 
+("CEO", 2000000, 1),
+("CTO", 1500000, 1),
+("COO", 1500000, 1),
+("Eng Head", 150000,2)
+("Ops Head", 150000,3)
+("Engineer", 100000, 2),
+("Operator", 90000, 3),
+("Finance", 90000, 2),
+("Finance", 90000, 3);
 
-INSERT INTO
-    employee (first_name, last_name, role_id) VALUE ("Albert", "Cheng", 1);
 
-INSERT INTO
-    employee (first_name, last_name, role_id, manager_id) VALUE ("John", "Smith", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUE 
+("Raj", "Batra", 1 , NULL),
+("Kavi", "Karr", 2, 1), 
+("Mani", "Word", 3, 1), 
+("Murugan", "Sanders",4, 2), 
+("Alpha","Kang", 5, 3), 
+("Kannan","Gupta", 6, 4), 
+("Albert", "Cheng", 7, 5),
+("Winnie", "Zhang", 8, 4),
+("Kyle", "Chan", 9, 5);
